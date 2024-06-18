129910db6f3dba25a79fec48a8511b3a4e2fa91f
Pratheek KV <30770392+kvpratheek@users.noreply.github.com>
2024-06-18 14:53:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement("h1",{},"Hello World React");
 
-const heading1 = React.createElement("h2",{},"Hello World React2");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-root.render(heading1);
+//JSX Element ( React Element)
+const jsxHeading = <h1>Heading with JSX { 100 *10 } </h1>;
+
+const jsxTitle = <h1>Heading with JSX</h1>;
+
+const HeadingComponent = () => (
+    <div>
+        {jsxHeading}
+        <h1>Heading with Component</h1>
+    </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<HeadingComponent />);
+
